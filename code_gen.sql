7523adc8ddfb801fce820eb339c48535c150063e
Ken Tran <ken.tran@qrious.co.nz>
2025-10-03 03:18:59 +0000

    starting on code gen

--- code_gen.sql
+++ code_gen.sql
@@ -1,1 +1,5 @@
-{{ codegen.generate_source(schema_name= 'pos',table_names = ["orders"], database_name= 'raw',generate_columns = true) }}
+/*
+1. Generate source yml file
+dbt run-operation generate_source --args '{"schema_name": "pos", "database_name": "raw", "generate_columns": true, "include_data_types": true, "include_descriptions":true, "include_schema": true}'
+
+*/
